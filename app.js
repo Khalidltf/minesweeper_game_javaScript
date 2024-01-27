3ed45af29360a7097368b1b11625a7b4ccd7bcb1
khalidlotfi <khalidlotfi2005@gmail.com>
2024-01-27 16:24:18 +0100

    WIP

--- app.js
+++ app.js
@@ -1,22 +1,60 @@
-
-
-
-let ROWS = 9;
-let COLS = 9;
+let ROWS = 2;
+let COLS = 2;
 let SIZE = 24;
 let canvas = document.getElementById("canvas");
 
+let cells = new Map();
+let revealedKeys = new Set();
+let values = new Map([
+  ["0-0", 1],
+  ["0-1", 1],
+  ["1-0", 1],
+  ["1-1", 1],
+]);
+
+function toKey(row, col) {
+  return row + "-" + col;
+}
+
 function createButtons() {
-  canvas.style.width = ROWS * SIZE;
-  canvas.style.height = COLS * SIZE;
+  canvas.style.width = ROWS * SIZE + "px";
+  canvas.style.height = COLS * SIZE + "px";
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
-      let btn = document.createElement("button");
-      btn.style.width = SIZE;
-      btn.style.height = SIZE;
-      canvas.appendChild(btn);
+      let cell = document.createElement("button");
+      cell.style.float = 'left'
+      cell.style.width = SIZE + "px";
+      cell.style.height = SIZE + "px";
+      cell.onclick = () => {
+        revealCell(key);
+      };
+      canvas.appendChild(cell);
+      let key = toKey(i, j);
+      cells.set(key, cell);
     }
   }
+}
+
+function updateButtons() {
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      let key = toKey(i, j);
+      let cell = cells.get(key);
+      if (revealedKeys.has(key)) {
+        cell.disabled = true;
+        let value = values.get(key);
+        cell.textContent = value.toString();
+      } else {
+        cell.disabled = false;
+        cell.textContent = "";
+      }
+    }
+  }
+}
+
+function revealCell(key) {
+  revealedKeys.add(key);
+  updateButtons();
 }
 
 createButtons();
